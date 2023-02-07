0da73c33f201f824df677a8c2937cab4cb84e87c
ostapvasilenko <ostapvasilenko55@gmail.com>
2023-02-07 15:19:16 +0200

    1

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,29 @@
-let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
-  });
-});
+/* Перше */
+
+/*const NUMBER = Number(prompt('Введіть ваше число!'))
+const DEGREE = Number(prompt('Введіть ступінь до якого хочете піднести число!'))
+let exponent = function (NUMBER, DEGREE) {
+
+  if (NUMBER < 0 || DEGREE < 0) {
+    alert('Ви ввели від’ємне число!!!')
+  } else{
+    return (DEGREE === 1) ? NUMBER : (NUMBER * exponent(NUMBER, DEGREE - 1))
+  }
+}
+
+alert(`Ваше число піднесене до степеню = ${exponent(NUMBER, DEGREE)}`);*/
+
+/* Друге */
+
+/*const FIRSTNUMBER = Number(prompt('Введіть перше число'))
+const LASTNUMBER = Number(prompt('Введіть друге число'))
+let gcd = function (FIRSTNUMBER, LASTNUMBER) {
+  if (FIRSTNUMBER < 0 || LASTNUMBER < 0) {
+    alert('Ви ввели від’ємне число!!!')
+  } else {
+    return (!LASTNUMBER) ? FIRSTNUMBER : gcd(LASTNUMBER, FIRSTNUMBER % LASTNUMBER)
+  }
+
+}
+alert(`Найбільший спільний дільник введених вами чисел = ${gcd(FIRSTNUMBER, LASTNUMBER)}`);*/
 
